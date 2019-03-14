ce655963ad0ecbfcb420385c3abdf35644df03a3
三上大河 <mikamimikami60@gmail.com>
2019-03-14 21:07:43 +0900

    repl追加

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"github.com/TaigaMikami/monkey/repl"
+	"os"
+	"os/user"
+)
+
+func main() {
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Hello %s! This is the Monkey programming language!\n",
+		user.Username)
+	fmt.Println("Feel free to type in commands\n")
+	repl.Start(os.Stdin, os.Stdout)
+}
